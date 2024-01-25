214e0418933360e0a7ef4f0de504a6c55322a8b9
eom-tae-in <eti0728@daum.net>
2024-01-25 10:10:58 +0900

    refactor: 불필요한 로직 제거
    Co-authored-by: devholic22 <hyunjoon.tech@gmail.com>

--- src/main/java/com/atwoz/member/ui/auth/support/oauth/OAuthAdapter.java
+++ src/main/java/com/atwoz/member/ui/auth/support/oauth/OAuthAdapter.java
@@ -1,6 +1,5 @@
 package com.atwoz.member.ui.auth.support.oauth;
 
-import com.atwoz.member.ui.auth.support.oauth.OAuthProperties.Provider;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,8 +9,6 @@ public class OAuthAdapter {
     }
 
     public static Map<String, OAuthProvider> getOauthProviders(final OAuthProperties properties) {
-        Map<String, Provider> provider = properties.getProvider();
-        System.out.println("aaaa" + provider.keySet());
 
         Map<String, OAuthProvider> oauthProvider = new HashMap<>();
 

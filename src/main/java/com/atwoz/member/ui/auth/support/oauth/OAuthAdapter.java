49bcde88175d233455b1d76c3c1b1eef21d2b54d
eom-tae-in <eti0728@daum.net>
2024-01-25 10:19:00 +0900

    refactor: 기본 생성자를 어노테이션으로 대체
    Co-authored-by: devholic22 <hyunjoon.tech@gmail.com>

--- src/main/java/com/atwoz/member/ui/auth/support/oauth/OAuthAdapter.java
+++ src/main/java/com/atwoz/member/ui/auth/support/oauth/OAuthAdapter.java
@@ -2,14 +2,13 @@ package com.atwoz.member.ui.auth.support.oauth;
 
 import java.util.HashMap;
 import java.util.Map;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class OAuthAdapter {
 
-    private OAuthAdapter() {
-    }
-
     public static Map<String, OAuthProvider> getOauthProviders(final OAuthProperties properties) {
-
         Map<String, OAuthProvider> oauthProvider = new HashMap<>();
 
         properties.getUser().forEach((key, value) -> oauthProvider.put(key,

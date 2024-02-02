02185c659e9600722cfef354e78dbd1c7e5617bc
eom-tae-in <eti0728@daum.net>
2024-02-02 14:16:16 +0900

    refactor: 기존의 http 요청 메세지 관련 작업을 RequestProcessor에서 하도록 변경
    Co-authored-by: devholic22 <hyunjoon.tech@gmail.com>

--- src/main/java/com/atwoz/member/ui/auth/support/resolver/OAuthArgumentResolver.java
+++ src/main/java/com/atwoz/member/ui/auth/support/resolver/OAuthArgumentResolver.java
@@ -2,12 +2,9 @@ package com.atwoz.member.ui.auth.support.resolver;
 
 import com.atwoz.member.ui.auth.support.oauth.OAuthAuthority;
 import com.atwoz.member.ui.auth.support.oauth.OAuthProperties;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -16,11 +13,11 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 @RequiredArgsConstructor
-@EnableConfigurationProperties(OAuthProperties.class)
 @Component
 public class OAuthArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final OAuthProperties oAuthProperties;
+    private final RequestProcessor requestProcessor;
 
     @Override
     public boolean supportsParameter(final MethodParameter parameter) {
@@ -31,18 +28,10 @@ public class OAuthArgumentResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(final MethodParameter parameter,
                                   final ModelAndViewContainer mavContainer,
                                   final NativeWebRequest webRequest,
-                                  final WebDataBinderFactory binderFactory) throws Exception {
+                                  final WebDataBinderFactory binderFactory) throws IOException {
         HttpServletRequest httpServletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = httpServletRequest.getReader();
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            stringBuilder.append(line);
-        }
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(stringBuilder.toString());
-        String provider = jsonNode.get("provider").asText();
+        String providerName = requestProcessor.readJsonFromBody(httpServletRequest);
 
-        return oAuthProperties.findByProviderName(provider);
+        return oAuthProperties.findByProviderName(providerName);
     }
 }

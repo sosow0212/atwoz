3061f4cfa7657262e962e4bda1ca3bb6d49de570
eom-tae-in <eti0728@daum.net>
2024-02-10 15:31:49 +0900

    remove: 불필요해진 클래스 제거

--- src/main/java/com/atwoz/member/ui/auth/support/auth/RequestProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.atwoz.member.ui.auth.support.auth;
-
-import jakarta.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-public interface RequestProcessor {
-
-    String readJsonFromBody(HttpServletRequest httpServletRequest) throws IOException;
-}

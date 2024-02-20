db4e702a7361281f96088c7d2ae12ba9943de5b2
devholic22 <hyunjoon.tech@gmail.com>
2024-02-20 23:51:26 +0900

    refactor: Member에서 Hobbies, Styles 삭제
    - Member에서 Hobbies, Styles에 대한 의존 삭제

--- src/main/java/com/atwoz/member/application/member/MemberService.java
+++ src/main/java/com/atwoz/member/application/member/MemberService.java
@@ -1,10 +1,7 @@
 package com.atwoz.member.application.member;
 
-import com.atwoz.member.domain.info.hobby.Hobbies;
-import com.atwoz.member.domain.info.style.Styles;
 import com.atwoz.member.domain.member.Member;
 import com.atwoz.member.domain.member.MemberRepository;
-import com.atwoz.member.exception.exceptions.member.MemberNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,22 +17,5 @@ public class MemberService {
         if (!memberRepository.existsByEmail(email)) {
             memberRepository.save(Member.createWithOAuthLogin(email, nickname));
         }
-    }
-
-    @Transactional
-    public void updateHobbies(final Long memberId, final Hobbies hobbies) {
-        Member member = findById(memberId);
-        member.updateHobbies(hobbies);
-    }
-
-    private Member findById(final Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(MemberNotFoundException::new);
-    }
-
-    @Transactional
-    public void updateStyles(final Long memberId, final Styles styles) {
-        Member member = findById(memberId);
-        member.updateStyles(styles);
     }
 }

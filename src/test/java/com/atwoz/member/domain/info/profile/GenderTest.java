be7ce4dfa64b4b6fb5dac3b3a3884836b02bee7c
devholic22 <hyunjoon.tech@gmail.com>
2024-02-22 22:01:09 +0900

    test: Gender 테스트 작성
    - Gender 테스트 작성

---
+++ src/test/java/com/atwoz/member/domain/info/profile/GenderTest.java
@@ -0,0 +1,33 @@
+package com.atwoz.member.domain.info.profile;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import com.atwoz.global.exception.exceptions.PropertyNotFoundException;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+public class GenderTest {
+
+    @Test
+    void 올바른_선택_시_조회된다() {
+        // given
+        String genderName = "남성";
+
+        // when & then
+        assertDoesNotThrow(() -> Gender.findByName(genderName));
+    }
+
+    @Test
+    void 올바른_성별이_아니면_예외가_발생한다() {
+        // given
+        String genderName = "hello";
+
+        // when & then
+        assertThatThrownBy(() -> Gender.findByName(genderName))
+                .isInstanceOf(PropertyNotFoundException.class);
+    }
+}

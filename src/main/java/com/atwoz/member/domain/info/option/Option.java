a2ef0d975c972df44b891fdb1604acdfab8775da
devholic22 <hyunjoon.tech@gmail.com>
2024-02-23 02:40:13 +0900

    test: OptionJpaRepository 테스트 작성
    - OptionJpaRepository 테스트 작성
    - Option Equals&HashCode 정의

--- src/main/java/com/atwoz/member/domain/info/option/Option.java
+++ src/main/java/com/atwoz/member/domain/info/option/Option.java
@@ -9,10 +9,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@EqualsAndHashCode(exclude = "id")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "member_option")
 @Entity

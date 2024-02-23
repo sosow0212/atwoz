5194b38eac77fa770d369b7a9b270339913bcd09
devholic22 <hyunjoon.tech@gmail.com>
2024-02-24 03:31:12 +0900

    refactor: Profile 존재 여부 메서드 성능 개선
    - ProfileRepository의 isExistMemberOption을 쿼리로 개선

--- src/main/java/com/atwoz/member/infrastructure/info/profile/ProfileJpaRepository.java
+++ src/main/java/com/atwoz/member/infrastructure/info/profile/ProfileJpaRepository.java
@@ -2,9 +2,13 @@ package com.atwoz.member.infrastructure.info.profile;
 
 import com.atwoz.member.domain.info.profile.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface ProfileJpaRepository extends JpaRepository<Profile, Long> {
 
     Optional<Profile> findByMemberId(final Long memberId);
+
+    @Query("select count(*) > 0 from Profile p where p.memberId = :memberId")
+    boolean isExistMemberOption(final Long memberId);
 }
--- src/main/java/com/atwoz/member/infrastructure/info/profile/ProfileRepositoryImpl.java
+++ src/main/java/com/atwoz/member/infrastructure/info/profile/ProfileRepositoryImpl.java
@@ -24,6 +24,6 @@ public class ProfileRepositoryImpl implements ProfileRepository {
 
     @Override
     public boolean isExistMemberProfile(final Long memberId) {
-        return findByMemberId(memberId).isPresent();
+        return profileJpaRepository.isExistMemberOption(memberId);
     }
 }

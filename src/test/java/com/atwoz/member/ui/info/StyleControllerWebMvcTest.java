f35e9c6f8f34385f4bc9d2621f65f84374c27855
devholic22 <hyunjoon.tech@gmail.com>
2024-02-23 15:18:43 +0900

    test: StyleControllerWebMvc 테스트 작성
    - StyleControllerWebMvc 테스트 작성
    - REST Docs 연결 작성

---
+++ src/test/java/com/atwoz/member/ui/info/StyleControllerWebMvcTest.java
@@ -0,0 +1,86 @@
+package com.atwoz.member.ui.info;
+
+import static com.atwoz.helper.RestDocsHelper.customDocument;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.atwoz.member.application.info.style.StyleService;
+import com.atwoz.member.domain.info.style.Style;
+import com.atwoz.member.domain.info.style.StyleName;
+import com.atwoz.member.ui.auth.interceptor.LoginValidCheckerInterceptor;
+import com.atwoz.member.ui.auth.interceptor.ParseMemberIdFromTokenInterceptor;
+import com.atwoz.member.ui.auth.support.resolver.AuthArgumentResolver;
+import com.atwoz.member.ui.auth.support.resolver.OAuthArgumentResolver;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.List;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+@AutoConfigureRestDocs
+@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(StyleController.class)
+public class StyleControllerWebMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StyleService styleService;
+
+    @MockBean
+    private AuthArgumentResolver authArgumentResolver;
+
+    @MockBean
+    private ParseMemberIdFromTokenInterceptor parseMemberIdFromTokenInterceptor;
+
+    @MockBean
+    private LoginValidCheckerInterceptor loginValidCheckerInterceptor;
+
+    @MockBean
+    private OAuthArgumentResolver oAuthArgumentResolver;
+
+    @Test
+    void 회원의_스타일을_조회한다() throws Exception {
+        // given
+        Long memberId = 1L;
+
+        when(oAuthArgumentResolver.supportsParameter(any())).thenReturn(true);
+        when(parseMemberIdFromTokenInterceptor.preHandle(any(), any(), any())).thenReturn(true);
+        when(loginValidCheckerInterceptor.preHandle(any(), any(), any())).thenReturn(true);
+        when(authArgumentResolver.supportsParameter(any())).thenReturn(true);
+        when(authArgumentResolver.resolveArgument(any(), any(), any(), any())).thenReturn(memberId);
+
+        List<Style> savedStyles = List.of(
+                new Style(memberId, StyleName.POSITIVE),
+                new Style(memberId, StyleName.GENTLE)
+        );
+        when(styleService.findMemberStyles(memberId)).thenReturn(savedStyles);
+
+        // when & then
+        mockMvc.perform(get("/api/info/style"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].style").value("긍정적"))
+                .andExpect(jsonPath("$[1].style").value("진중함"))
+                .andDo(print())
+                .andDo(customDocument("search_styles",
+                        responseFields(
+                                fieldWithPath("[].style").description("스타일 이름")
+                        )
+                ));
+    }
+}

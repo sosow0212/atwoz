57303b23d6fee5dc946f9528eecc4d1f1e220747
devholic22 <hyunjoon.tech@gmail.com>
2024-02-29 04:00:15 +0900

    refactor: PositionTest 인자 수정
    - 예외 검증 시 double 인자로 검증되도록 수정

--- src/test/java/com/atwoz/member/domain/info/profile/PositionTest.java
+++ src/test/java/com/atwoz/member/domain/info/profile/PositionTest.java
@@ -27,11 +27,11 @@ class PositionTest {
     }
 
     @ParameterizedTest(name = "위도가 {0}인 경우")
-    @ValueSource(ints = {-100, 100})
-    void 위도_제한을_벗어나면_예외가_발생한다(final int lat) {
+    @ValueSource(doubles = {-100.23, 100.34})
+    void 위도_제한을_벗어나면_예외가_발생한다(final double lat) {
         // given
         BigDecimal latitude = BigDecimal.valueOf(lat);
-        BigDecimal longitude = BigDecimal.valueOf(130);
+        BigDecimal longitude = BigDecimal.valueOf(130.21);
 
         // when & then
         assertThatThrownBy(() -> new Position(latitude, longitude))
@@ -39,10 +39,10 @@ class PositionTest {
     }
 
     @ParameterizedTest(name = "경도가 {0}인 경우")
-    @ValueSource(ints = {-200, 200})
-    void 경도_제한을_벗어나면_예외가_발생한다(final int lon) {
+    @ValueSource(doubles = {-200.29, 200.57})
+    void 경도_제한을_벗어나면_예외가_발생한다(final double lon) {
         // given
-        BigDecimal latitude = BigDecimal.valueOf(40);
+        BigDecimal latitude = BigDecimal.valueOf(40.34);
         BigDecimal longitude = BigDecimal.valueOf(lon);
 
         // when & then

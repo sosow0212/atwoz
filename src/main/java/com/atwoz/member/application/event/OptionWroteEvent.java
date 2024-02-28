bca409a5d93cb5c4de8251413b8cb8a0af0e4b1e
devholic22 <hyunjoon.tech@gmail.com>
2024-02-28 21:00:48 +0900

    refactor: 이벤트 RequiredArgsConstructor 통일
    - 이벤트들의 통일을 위해 RequiredArgsConstructor 추가

--- src/main/java/com/atwoz/member/application/event/OptionWroteEvent.java
+++ src/main/java/com/atwoz/member/application/event/OptionWroteEvent.java
@@ -3,16 +3,12 @@ package com.atwoz.member.application.event;
 import com.atwoz.global.event.Event;
 import com.atwoz.member.application.info.dto.option.OptionWriteRequest;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class OptionWroteEvent extends Event {
 
     private final Long memberId;
     private final OptionWriteRequest request;
-
-    public OptionWroteEvent(final Long memberId, final OptionWriteRequest request) {
-        super();
-        this.memberId = memberId;
-        this.request = request;
-    }
 }
--- src/main/java/com/atwoz/member/application/event/ProfileWroteEvent.java
+++ src/main/java/com/atwoz/member/application/event/ProfileWroteEvent.java
@@ -3,16 +3,12 @@ package com.atwoz.member.application.event;
 import com.atwoz.global.event.Event;
 import com.atwoz.member.application.info.dto.profile.ProfileWriteRequest;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class ProfileWroteEvent extends Event {
 
     private final Long memberId;
     private final ProfileWriteRequest request;
-
-    public ProfileWroteEvent(final Long memberId, final ProfileWriteRequest request) {
-        super();
-        this.memberId = memberId;
-        this.request = request;
-    }
 }

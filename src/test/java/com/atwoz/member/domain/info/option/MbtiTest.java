c483c347824d2ddfd0669c494ea284087006e8fc
devholic22 <hyunjoon.tech@gmail.com>
2024-02-28 17:29:31 +0900

    test: Mbti 테스트 작성
    - Mbti 테스트 작성

---
+++ src/test/java/com/atwoz/member/domain/info/option/MbtiTest.java
@@ -0,0 +1,33 @@
+package com.atwoz.member.domain.info.option;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import com.atwoz.member.exception.exceptions.info.option.MbtiInvalidException;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+public class MbtiTest {
+
+    @Test
+    void 올바른_선택_시_조회된다() {
+        // given
+        String mbtiName = "INFJ";
+
+        // when & then
+        assertDoesNotThrow(() -> Mbti.findByName(mbtiName));
+    }
+
+    @Test
+    void 올바른_mbti가_아니면_예외가_발생한다() {
+        // given
+        String mbtiName = "hello";
+
+        // when & then
+        assertThatThrownBy(() -> Mbti.findByName(mbtiName))
+                .isInstanceOf(MbtiInvalidException.class);
+    }
+}

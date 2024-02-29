83241e137632cb8a56bd014a31b1d8e8c0ae92c2
devholic22 <hyunjoon.tech@gmail.com>
2024-02-29 13:03:40 +0900

    fix: OptionRequest 구현체 DTO 기본 생성자 추가
    - OptionRequest 구현체 DTO 기본 생성자 추가

--- src/main/java/com/atwoz/member/application/info/dto/option/OptionUpdateRequest.java
+++ src/main/java/com/atwoz/member/application/info/dto/option/OptionUpdateRequest.java
@@ -1,10 +1,13 @@
 package com.atwoz.member.application.info.dto.option;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class OptionUpdateRequest implements OptionRequest {
 

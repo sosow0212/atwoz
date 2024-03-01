1efb1f6e58b105ba41466b7f3cedf207d8d9a03b
devholic22 <hyunjoon.tech@gmail.com>
2024-03-02 03:53:07 +0900

    refactor: MemberHobby, MemberStyle 삭제 쿼리 개선
    - MemberHobby, MemberStyle 삭제 쿼리 개선

--- src/main/java/com/atwoz/member/infrastructure/info/hobby/MemberHobbyJpaRepository.java
+++ src/main/java/com/atwoz/member/infrastructure/info/hobby/MemberHobbyJpaRepository.java
@@ -2,10 +2,14 @@ package com.atwoz.member.infrastructure.info.hobby;
 
 import com.atwoz.member.domain.info.hobby.MemberHobby;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface MemberHobbyJpaRepository extends JpaRepository<MemberHobby, Long> {
 
+    @Modifying
+    @Query("delete from MemberHobby mh where mh.memberId = :memberId")
     void deleteByMemberId(final Long memberId);
 
     List<MemberHobby> findAllByMemberId(final Long memberId);
--- src/main/java/com/atwoz/member/infrastructure/info/style/MemberStyleJpaRepository.java
+++ src/main/java/com/atwoz/member/infrastructure/info/style/MemberStyleJpaRepository.java
@@ -2,10 +2,15 @@ package com.atwoz.member.infrastructure.info.style;
 
 import com.atwoz.member.domain.info.style.MemberStyle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface MemberStyleJpaRepository extends JpaRepository<MemberStyle, Long> {
 
+    @Modifying
+    @Query("delete from MemberStyle ms where ms.memberId = :memberId")
     void deleteByMemberId(final Long memberId);
+
     List<MemberStyle> findAllByMemberId(final Long memberId);
 }

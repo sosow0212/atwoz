654235e20ed0cd6147810bfb657b4cdd88735710
devholic22 <hyunjoon.tech@gmail.com>
2024-03-02 04:03:07 +0900

    test: InfoController 인수 테스트 추가 작성
    - 회원 정보 조회 예외 검증 추가
    - 회원 정보 수정 검증 추가
    - 회원 정보 수정 예외 검증 추가

--- src/test/java/com/atwoz/member/ui/info/InfoControllerAcceptanceFixture.java
+++ src/test/java/com/atwoz/member/ui/info/InfoControllerAcceptanceFixture.java
@@ -1,13 +1,19 @@
 package com.atwoz.member.ui.info;
 
 import static com.atwoz.member.fixture.info.MemberHobbiesFixture.회원_일반_취미_생성;
+import static com.atwoz.member.fixture.info.MemberStylesFixture.회원_일반_스타일_생성;
 import static com.atwoz.member.fixture.info.OptionFixture.회원_일반_옵션_생성;
 import static com.atwoz.member.fixture.info.ProfileFixture.회원_일반_프로필_생성;
-import static com.atwoz.member.fixture.info.MemberStylesFixture.회원_일반_스타일_생성;
+import static com.atwoz.member.fixture.info.dto.response.HobbiesSearchResponseFixture.회원_취미_조회_응답;
+import static com.atwoz.member.fixture.info.dto.response.OptionSearchResponseFixture.회원_옵션_조회_응답;
+import static com.atwoz.member.fixture.info.dto.response.ProfileSearchResponseFixture.회원_프로필_조회_응답;
+import static com.atwoz.member.fixture.info.dto.response.StylesSearchResponseFixture.회원_스타일_조회_응답;
 import static com.atwoz.member.fixture.member.MemberFixture.일반_유저_생성;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import com.atwoz.helper.IntegrationHelper;
+import com.atwoz.member.application.info.dto.InfoUpdateRequest;
 import com.atwoz.member.application.info.dto.InfoWriteRequest;
 import com.atwoz.member.domain.info.hobby.MemberHobbyRepository;
 import com.atwoz.member.domain.info.option.OptionRepository;
@@ -16,6 +22,7 @@ import com.atwoz.member.domain.info.style.MemberStyleRepository;
 import com.atwoz.member.domain.member.Member;
 import com.atwoz.member.domain.member.MemberRepository;
 import com.atwoz.member.infrastructure.auth.JwtTokenProvider;
+import com.atwoz.member.ui.info.dto.InfoSearchResponse;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
@@ -76,6 +83,25 @@ class InfoControllerAcceptanceFixture extends IntegrationHelper {
         memberStyleRepository.saveAll(회원_일반_스타일_생성());
     }
 
+    protected ExtractableResponse 회원_정보를_수정한다(final String token, final InfoUpdateRequest request, final String url) {
+        return RestAssured.given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .patch(url)
+                .then().log().all()
+                .extract();
+    }
+
+    protected void 회원_정보_수정_검증(final ExtractableResponse response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    protected void 회원_정보_수정_예외_검증(final ExtractableResponse response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
     protected ExtractableResponse 회원_정보를_조회한다(final String token, final String url) {
         return RestAssured.given().log().all()
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
@@ -86,6 +112,17 @@ class InfoControllerAcceptanceFixture extends IntegrationHelper {
     }
 
     protected void 회원_정보_조회_결과_검증(final ExtractableResponse response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        InfoSearchResponse result = response.as(InfoSearchResponse.class);
+        assertSoftly(softly -> {
+            softly.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            softly.assertThat(result.profile()).isEqualTo(회원_프로필_조회_응답());
+            softly.assertThat(result.option()).isEqualTo(회원_옵션_조회_응답());
+            softly.assertThat(result.hobbies()).isEqualTo(회원_취미_조회_응답());
+            softly.assertThat(result.styles()).isEqualTo(회원_스타일_조회_응답());
+        });
+    }
+
+    protected void 회원_정보_조회_결과_예외_검증(final ExtractableResponse response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 }
--- src/test/java/com/atwoz/member/ui/info/InfoControllerAcceptanceTest.java
+++ src/test/java/com/atwoz/member/ui/info/InfoControllerAcceptanceTest.java
@@ -1,6 +1,7 @@
 package com.atwoz.member.ui.info;
 
-import static com.atwoz.member.fixture.info.dto.request.InfoWriteRequestFixture.회원_정보_생성_요청;
+import static com.atwoz.member.fixture.info.dto.request.InfoRequestFixture.회원_정보_생성_요청;
+import static com.atwoz.member.fixture.info.dto.request.InfoRequestFixture.회원_정보_수정_요청;
 
 import com.atwoz.member.domain.member.Member;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Test;
 class InfoControllerAcceptanceTest extends InfoControllerAcceptanceFixture {
 
     private static final String 회원_정보_생성_요청_url = "/api/info";
+    private static final String 회원_정보_수정_요청_url = "/api/info";
     private static final String 회원_정보_조회_요청_url = "/api/info";
 
     private Member 회원;
@@ -37,6 +39,31 @@ class InfoControllerAcceptanceTest extends InfoControllerAcceptanceFixture {
     }
 
     @Test
+    void 회원의_정보를_수정한다() {
+        // given
+        회원_정보_생성();
+        var 회원_정보_수정_요청서 = 회원_정보_수정_요청();
+
+        // when
+        var 회원_정보_수정_요청_결과 = 회원_정보를_수정한다(토큰, 회원_정보_수정_요청서, 회원_정보_수정_요청_url);
+
+        // then
+        회원_정보_수정_검증(회원_정보_수정_요청_결과);
+    }
+
+    @Test
+    void 회원_정보가_없으면_수정할_수_없다() {
+        // given
+        var 회원_정보_수정_요청서 = 회원_정보_수정_요청();
+
+        // when
+        var 회원_정보_수정_요청_결과 = 회원_정보를_수정한다(토큰, 회원_정보_수정_요청서, 회원_정보_수정_요청_url);
+
+        // then
+        회원_정보_수정_예외_검증(회원_정보_수정_요청_결과);
+    }
+
+    @Test
     void 회원의_정보를_조회한다() {
         // given
         회원_정보_생성();
@@ -46,5 +73,14 @@ class InfoControllerAcceptanceTest extends InfoControllerAcceptanceFixture {
 
         // then
         회원_정보_조회_결과_검증(회원_정보_조회_결과);
+    }
+
+    @Test
+    void 회원_정보가_없으면_조회할_수_없다() {
+        // when
+        var 회원_정보_조회_결과 = 회원_정보를_조회한다(토큰, 회원_정보_조회_요청_url);
+
+        // then
+        회원_정보_조회_결과_예외_검증(회원_정보_조회_결과);
     }
 }

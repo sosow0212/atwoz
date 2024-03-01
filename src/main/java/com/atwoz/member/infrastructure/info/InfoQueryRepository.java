1e41618863fd9cb63d5854a42cceab52fc61c398
devholic22 <hyunjoon.tech@gmail.com>
2024-03-01 22:37:42 +0900

    refactor: Querydsl 구현체 분리
    - InfoRepositoryImpl과 JPAQueryFactory 분리되도록 수정

---
+++ src/main/java/com/atwoz/member/infrastructure/info/InfoQueryRepository.java
@@ -0,0 +1,76 @@
+package com.atwoz.member.infrastructure.info;
+
+import static com.atwoz.member.domain.info.hobby.QMemberHobby.memberHobby;
+import static com.atwoz.member.domain.info.option.QOption.option;
+import static com.atwoz.member.domain.info.profile.QProfile.profile;
+import static com.atwoz.member.domain.info.style.QMemberStyle.memberStyle;
+
+import com.atwoz.member.domain.info.option.Option;
+import com.atwoz.member.domain.info.profile.Profile;
+import com.atwoz.member.exception.exceptions.info.option.OptionNotFoundException;
+import com.atwoz.member.exception.exceptions.info.profile.ProfileNotFoundException;
+import com.atwoz.member.ui.info.dto.HobbySearchResponse;
+import com.atwoz.member.ui.info.dto.InfoSearchResponse;
+import com.atwoz.member.ui.info.dto.StyleSearchResponse;
+import com.atwoz.member.ui.info.dto.option.OptionSearchResponse;
+import com.atwoz.member.ui.info.dto.profile.ProfileSearchResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class InfoQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public InfoSearchResponse findByMemberId(final Long memberId) {
+        Profile profile = selectProfileByMemberId(memberId);
+        Option option = selectOptionByMemberId(memberId);
+        ProfileSearchResponse profileSearchResponse = ProfileSearchResponse.from(profile);
+        OptionSearchResponse optionSearchResponse = OptionSearchResponse.from(option);
+
+        List<HobbySearchResponse> hobbyCodes = selectHobbyResponseByMemberId(memberId);
+        List<StyleSearchResponse> styleCodes = selectStyleResponseByMemberId(memberId);
+
+        return new InfoSearchResponse(profileSearchResponse, optionSearchResponse, hobbyCodes, styleCodes);
+    }
+
+    private Profile selectProfileByMemberId(final Long memberId) {
+        return Optional.ofNullable(queryFactory.select(profile)
+                        .from(profile)
+                        .where(profile.memberId.eq(memberId))
+                        .fetchOne())
+                .orElseThrow(ProfileNotFoundException::new);
+    }
+
+    private Option selectOptionByMemberId(final Long memberId) {
+        return Optional.ofNullable(queryFactory.select(option)
+                        .from(option)
+                        .where(option.memberId.eq(memberId))
+                        .fetchOne())
+                .orElseThrow(OptionNotFoundException::new);
+    }
+
+    private List<HobbySearchResponse> selectHobbyResponseByMemberId(final Long memberId) {
+        return queryFactory.select(memberHobby)
+                .from(memberHobby)
+                .where(memberHobby.memberId.eq(memberId))
+                .fetch()
+                .stream()
+                .map(HobbySearchResponse::from)
+                .toList();
+    }
+
+    private List<StyleSearchResponse> selectStyleResponseByMemberId(final Long memberId) {
+        return queryFactory.select(memberStyle)
+                .from(memberStyle)
+                .where(memberStyle.memberId.eq(memberId))
+                .fetch()
+                .stream()
+                .map(StyleSearchResponse::from)
+                .toList();
+    }
+}
